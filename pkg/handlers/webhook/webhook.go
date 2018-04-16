81a0ca4b54053ec8d21bf5a7060775537b175e34
Jorge Bianquetti <jbianquetti@bitnami.com>
2018-04-16 13:37:24 +0200

    using Copyright 2018 Bitnami for new files

--- pkg/handlers/webhook/webhook.go
+++ pkg/handlers/webhook/webhook.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 Skippbox, Ltd.
+Copyright 2018 Bitnami
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
