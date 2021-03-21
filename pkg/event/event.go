a55d028f5f4eb98e313b298b3666a96c5077725d
Natan Yellin <aantny@gmail.com>
2021-03-21 10:16:07 +0200

    remove unused code from event.go

--- pkg/event/event.go
+++ pkg/event/event.go
@@ -15,13 +15,6 @@ package event
 
 import (
 	"fmt"
-
-	"github.com/bitnami-labs/kubewatch/pkg/utils"
-	apps_v1 "k8s.io/api/apps/v1"
-	batch_v1 "k8s.io/api/batch/v1"
-	api_v1 "k8s.io/api/core/v1"
-	ext_v1beta1 "k8s.io/api/extensions/v1beta1"
-	rbac_v1beta1 "k8s.io/api/rbac/v1beta1"
 )
 
 // Event represent an event got from k8s api server
@@ -43,66 +36,6 @@ var m = map[string]string{
 	"updated": "Warning",
 }
 
-// New create new KubewatchEvent
-func New(obj interface{}, action string) Event {
-	var namespace, kind, component, host, reason, status, name string
-
-	objectMeta := utils.GetObjectMetaData(obj)
-	namespace = objectMeta.Namespace
-	name = objectMeta.Name
-	reason = action
-	status = m[action]
-
-	switch object := obj.(type) {
-	case *ext_v1beta1.DaemonSet:
-		kind = "daemon set"
-	case *apps_v1.Deployment:
-		kind = "deployment"
-	case *batch_v1.Job:
-		kind = "job"
-	case *api_v1.Namespace:
-		kind = "namespace"
-	case *ext_v1beta1.Ingress:
-		kind = "ingress"
-	case *api_v1.PersistentVolume:
-		kind = "persistent volume"
-	case *api_v1.Pod:
-		kind = "pod"
-		host = object.Spec.NodeName
-	case *api_v1.ReplicationController:
-		kind = "replication controller"
-	case *ext_v1beta1.ReplicaSet:
-		kind = "replica set"
-	case *api_v1.Service:
-		kind = "service"
-		component = string(object.Spec.Type)
-	case *api_v1.Secret:
-		kind = "secret"
-	case *api_v1.ConfigMap:
-		kind = "configmap"
-	case *api_v1.Node:
-		kind = "node"
-	case *rbac_v1beta1.ClusterRole:
-		kind = "cluster role"
-	case *api_v1.ServiceAccount:
-		kind = "service account"
-	case Event:
-		name = object.Name
-		kind = object.Kind
-		namespace = object.Namespace
-	}
-
-	kbEvent := Event{
-		Namespace: namespace,
-		Kind:      kind,
-		Component: component,
-		Host:      host,
-		Reason:    reason,
-		Status:    status,
-		Name:      name,
-	}
-	return kbEvent
-}
 
 // Message returns event message in standard format.
 // included as a part of event packege to enhance code resuablity across handlers.
